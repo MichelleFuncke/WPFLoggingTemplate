9468e1632116ab54973949f9feb22575a6950cbe
MICHELLE-TOWER\Michelle <kogelmichelle@gmail.com>
2017-04-21 20:16:09 +0200

    Basic Logging app

---
+++ WPFLoggingTemplate/WPFLoggingTemplate/Logging/ActionTracer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Purrfect.Logging
+{
+    public static class ActionTracer
+    {
+        private static int _maxEntries = 100;
+        private static Queue<string> _actions = new Queue<string>();
+
+        public static void PushAction(string context, params string[] argumentValues)
+        {
+            if (_actions.Count > _maxEntries)
+            {
+                _actions.Dequeue();
+            }
+
+            _actions.Enqueue(ActionToString(context, argumentValues));
+        }
+
+        private static string ActionToString(string context, string[] argumentValues)
+        {
+            StringBuilder ret = new StringBuilder(context);
+
+            foreach (var str in _actions)
+            {
+                ret.AppendFormat(", {0}", str);
+            }
+
+            return ret.ToString();
+        }
+
+        public static string ConvertToLogEntry()
+        {
+            StringBuilder ret = new StringBuilder();
+
+            foreach (var str in _actions)
+            {
+                ret.AppendLine(str);
+            }
+
+            return ret.ToString();
+        }
+    }
+}
---
+++ WPFLoggingTemplate/WPFLoggingTemplate/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Purrfect.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFLoggingTemplate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //So that I handle exceptions
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += new UnhandledExceptionEventHandler(MyHandler);
+
+            InitializeComponent();
+        }
+
+        static void MyHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            Exception e = (Exception)args.ExceptionObject;
+
+            Console.WriteLine("MyHandler caught : " + e.Message);
+            Console.WriteLine("Runtime terminating: {0}", args.IsTerminating);
+
+            //Log the info to a log file
+            string logs = "Logs\\Test_" + DateTime.Now.ToString("yyyy-MM-dd") + ".log";
+            string line = string.Format("{0};{1};{2} {3}\n(\n\tSource:{4}\n\tInnerException:{5}\n\tHResult:{6}\n\tStrackTrace:{7}\n)", DateTime.Now, Environment.UserName, "Test", e.Message, e.Source, e.InnerException, e.HResult, e.StackTrace);
+            //File.WriteAllText(logs, line);
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(logs, true))
+            {
+                //file.WriteLine(line);
+                file.WriteLine(ActionTracer.ConvertToLogEntry());
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            ActionTracer.PushAction("button", sender.ToString(), e.ToString());
+            throw new Exception("test");
+        }
+    }
+}
